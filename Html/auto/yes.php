8ba581dd2c5fb9b53f218e64d688e45fc3727961
Eemil74 <165161902+Eemil74@users.noreply.github.com>
2025-09-22 11:13:33 +0300

    Add files via upload

---
+++ Html/auto/yes.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="Fi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+            th
+            {
+                background-color: lightblue;
+            }
+            td
+            {
+                background-color:lightgray ;
+                width: 15%;
+            }
+            img
+            {
+                max-width: 160px;
+                max-height: 160px;
+            }
+        </style>
+</head>
+<body>
+<h2>Vehicle Showcase</h2>
+<table>
+    
+<tr>
+        <th>Auto malli</th>
+        <th>Moottorin Tilavuus</th>
+        <th>Kiihtyvyys</th>
+        <th>Huippunopeus</th>
+        <th>Kulutus (yhdistetty, maantie, kaupunki)</th>
+        <th>Image</th>
+    </tr>
+
+    <tr>
+        <td>Mercedes-Benz GLE</td>
+        <td>3,0 l</td>
+        <td>7,0 s / 100 km/h</td>
+        <td>226 km/h</td>
+        <td>7,2 / 6,6 / 7,9</td>
+        <td><img src="NJY-796-51.webp" alt="Vehicle 1"></td>
+    </tr>
+
+    <tr>
+        <td>Mercedes-Benz GLA</td>
+        <td>1,3 l</td>
+        <td>7,1 s / 100 km/h</td>
+        <td>235 km/h</td>
+        <td>1,1</td>
+        <td><img src="VAR-239246-1.webp" alt="Vehicle 2"></td>
+    </tr>
+
+</table>
+
+</body>
+</html>
